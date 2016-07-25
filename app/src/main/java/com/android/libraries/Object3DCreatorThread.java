0af3f74253ba66d17e3f149e42707afed8ccb51e
Mariollo14 <mario.salierno90@alice.it>
2016-07-25 17:48:14 +0200

    texturing issue solved

--- app/src/main/java/com/android/libraries/Object3DCreatorThread.java
+++ app/src/main/java/com/android/libraries/Object3DCreatorThread.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 /**
  * Created by Mario Salierno on 21/07/2016.
  */
-public class Object3DCreatorThread extends Thread{
+public class Object3DCreatorThread extends Thread {
 
     private String TAG = "Object3DCreatorThread";
 
@@ -27,72 +27,70 @@ public class Object3DCreatorThread extends Thread{
     private Location loc;
 
 
-    private String basename;
-    private String extension;
+    public Object3DCreatorThread(String message, JPCTWorldManager jpctWorldManager, Location loc) {
 
-
-    public Object3DCreatorThread(String message, JPCTWorldManager jpctWorldManager, Location loc){
-
-        this.message=message;
-        this.jpctWorldManager=jpctWorldManager;
-        this.loc=loc;
+        this.message = message;
+        this.jpctWorldManager = jpctWorldManager;
+        this.loc = loc;
 
     }
 
 
     @Override
-    public  void run(){
+    public void run() {
 
         try {
             Log.e(TAG, "run()");
-            Log.e(TAG,"message len:"+message.length());
-            Log.e(TAG, message.substring(0,20));
-            Log.e(TAG, message.substring(20,40));
-            Log.e(TAG, message.substring(40,60));
-            Log.e(TAG, message.substring(1398155,1398170));
+            Log.e(TAG, "message len:" + message.length());
+            Log.e(TAG, message.substring(0, 20));
+            //Log.e(TAG, message.substring(20,40));
+            //Log.e(TAG, message.substring(40,60));
+            //Log.e(TAG, message.substring(1398155,1398170));
             JSONObject job = new JSONObject(message);
 
             //Boolean successful = job.getBoolean("successful");
             Boolean successful = (Boolean) job.get("successful");
             Log.e(TAG, "successful not blocking");
-            Log.e(TAG, "successful:"+successful.toString());
-            if(successful){
-                String basename = job.getString("basename");
+            Log.e(TAG, "successful:" + successful.toString());
+            if (successful) {
+                //String basename = job.getString("basename");
+                //Log.e("basename", basename);
                 //String extension= job.getString("extension");
                 //String txtFile = job.getString("file");
-
-                byte[] txtr =  (org.apache.commons.codec.binary.Base64.decodeBase64(job.getString("file").getBytes()));
-                //byte[] txtr = job.getString("file").getBytes();
-                Log.e(TAG,"txtr len:"+txtr.length);
-                Texture texture = Object3DManager.deserializeTexture(txtr);
-
-
-                /*
-                Bitmap bmp = BitmapFactory.decodeByteArray(txtr, 0, txtr.length);
-                //Bitmap bmp = Bit
-                if(bmp==null)
-                    Log.e(TAG,"bmp==null");
-                Bitmap mutableBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
-
-                Texture texture = new Texture(mutableBitmap);
-                */
-                Log.e(TAG, "textured!");
-                if(texture==null)
-                    Log.e(TAG,"texture==null");
-                //texture = new Texture(200,200);
-                //texture = new Texture(BitmapHelper.convert(image));
-                TextureManager.getInstance().addTexture(basename, texture);
-                Log.e(TAG,"texture added to texturemanager");
 
                 try {
                     //Location loc = locationFusion.getAsynchBestLocationAmongLocators();
                     //loc.setLongitude(loc.getLongitude()+0.00002);
                     //byte[] obj3Ddata =  org.apache.commons.codec.binary.Base64.decodeBase64(job.getString("obj3d"));
-                    byte[] obj3Ddata =  (org.apache.commons.codec.binary.Base64.decodeBase64(job.getString("obj3d").getBytes()));
-                    Log.e(TAG,"obj3Ddata len:"+obj3Ddata.length);
+                    byte[] obj3Ddata = (org.apache.commons.codec.binary.Base64.decodeBase64(job.getString("obj3d").getBytes()));
+                    Log.e(TAG, "obj3Ddata len:" + obj3Ddata.length);
                     Object3D o3d = Object3DManager.deserializeObject3D(/*blob.array()*/obj3Ddata);
 
-                    jpctWorldManager.addObjectToCreationQueue(o3d,loc);
+                    Log.e(TAG, "o3d received name:" + o3d.getName());
+
+
+                    byte[] txtr = (org.apache.commons.codec.binary.Base64.decodeBase64(job.getString("file").getBytes()));
+                    //byte[] txtr = job.getString("file").getBytes();
+                    Log.e(TAG, "txtr len:" + txtr.length);
+
+                    Bitmap bmp = BitmapFactory.decodeByteArray(txtr, 0, txtr.length);
+                    //Bitmap bmp = Bit
+                    if(bmp==null)
+                        Log.e(TAG,"bmp==null");
+                    //Bitmap mutableBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
+
+                    Texture texture = new Texture(bmp);
+
+                    Log.e(TAG, "textured!");
+                    if (texture == null)
+                        Log.e(TAG, "texture==null");
+                    //texture = new Texture(200,200);
+                    //texture = new Texture(BitmapHelper.convert(image));
+                    TextureManager.getInstance().addTexture(o3d.getName() /*basename*/, texture);
+                    Log.e(TAG, "texture added to texturemanager");
+
+
+                    jpctWorldManager.addObjectToCreationQueue(o3d, loc);
                 } catch (IOException e) {
                     Log.e(TAG, "IOException");
                     e.printStackTrace();
@@ -102,22 +100,87 @@ public class Object3DCreatorThread extends Thread{
                     Log.e(TAG, e.getMessage());
                 }
 
-            }
-            else {
+            } else {
                 Log.e(TAG, job.getString("message"));
                 return;
             }
         } catch (JSONException e) {
             Log.e(TAG, "JSONException");
             Log.e(TAG, e.getMessage());
-        } catch (ClassNotFoundException e) {
-            Log.e(TAG, "JSONException");
-            Log.e(TAG, e.getMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
     }
+
+
+    /*
+    public void run2() {
+
+        try {
+            Log.e(TAG, "run()");
+            Log.e(TAG, "message len:" + message.length());
+            Log.e(TAG, message.substring(0, 20));
+            //Log.e(TAG, message.substring(20,40));
+            //Log.e(TAG, message.substring(40,60));
+            //Log.e(TAG, message.substring(1398155,1398170));
+            JSONObject job = new JSONObject(message);
+
+            //Boolean successful = job.getBoolean("successful");
+            Boolean successful = (Boolean) job.get("successful");
+            Log.e(TAG, "successful not blocking");
+            Log.e(TAG, "successful:" + successful.toString());
+            if (successful) {
+                //String basename = job.getString("basename");
+                //Log.e("basename", basename);
+                //String extension= job.getString("extension");
+                //String txtFile = job.getString("file");
+
+                try {
+                    //Location loc = locationFusion.getAsynchBestLocationAmongLocators();
+                    //loc.setLongitude(loc.getLongitude()+0.00002);
+                    //byte[] obj3Ddata =  org.apache.commons.codec.binary.Base64.decodeBase64(job.getString("obj3d"));
+                    byte[] obj3Ddata = (org.apache.commons.codec.binary.Base64.decodeBase64(job.getString("obj3d").getBytes()));
+                    Log.e(TAG, "obj3Ddata len:" + obj3Ddata.length);
+                    Object3D o3d = Object3DManager.deserializeObject3D(obj3Ddata);
+
+                    Log.e(TAG, "o3d received name:" + o3d.getName());
+
+
+                    byte[] txtr = (org.apache.commons.codec.binary.Base64.decodeBase64(job.getString("file").getBytes()));
+                    //byte[] txtr = job.getString("file").getBytes();
+                    Log.e(TAG, "txtr len:" + txtr.length);
+                    Texture texture = Object3DManager.deserializeTexture(txtr);
+
+
+                    Log.e(TAG, "textured!");
+                    if (texture == null)
+                        Log.e(TAG, "texture==null");
+                    //texture = new Texture(200,200);
+                    //texture = new Texture(BitmapHelper.convert(image));
+                    TextureManager.getInstance().addTexture(o3d.getName(), texture);
+                    Log.e(TAG, "texture added to texturemanager");
+
+
+                    jpctWorldManager.addObjectToCreationQueue(o3d, loc);
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException");
+                    e.printStackTrace();
+                    Log.e(TAG, e.getMessage());
+                } catch (ClassNotFoundException e) {
+                    Log.e(TAG, "ClassNotFoundException");
+                    Log.e(TAG, e.getMessage());
+                }
+
+            } else {
+                Log.e(TAG, job.getString("message"));
+                return;
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "JSONException");
+            Log.e(TAG, e.getMessage());
+        }
+
+    }
+        */
 
 
 }
